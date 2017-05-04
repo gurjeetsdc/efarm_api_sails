6d869c39e5a7bc2f7963193ca4929b6194a83a3b
Gurjeet Singh <gurjeets@smartdatainc.net>
2017-05-04 17:03:17 +0530

    updated response formats.

--- api/policies/OAuthValidateAccessToken.js
+++ api/policies/OAuthValidateAccessToken.js
@@ -1,6 +1,17 @@
 module.exports = function (req, res, next) {
     OAuth.authenticator.authenticate('bearer', { session: false }, function(err,identity,authorization) {
-        if (!identity ) return res.send(401);
+        if (!identity ) {
+        	
+        	var response = {
+                          	success: false,
+                          	error: {
+                              code: 401,
+                              message: 'authorization'
+                              
+                          	},
+                      	}
+        	return res.send(response);
+        }
 
         req.identity = identity;
         req.authorization = authorization;
